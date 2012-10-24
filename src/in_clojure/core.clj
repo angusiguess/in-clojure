159799cefdda81678b63be165005a483e9ff04b8
Angus Fletcher <ethdem@gmail.com>
2012-10-24 14:21:45 -0300

    Modify the player to use metronome
    -Modified the player to use the metronome.
    -Redid time signatures for the existing measures.
    -Introduced some new measures

--- src/in_clojure/core.clj
+++ src/in_clojure/core.clj
@@ -2,17 +2,35 @@
   (:use overtone.live
         overtone.inst.sampled-piano))
 
-(def part-one [:C5 :C5 :C5 :C5])
-(def part-one-time [300 300 300 300])
+(def rhythm [:C5 :C5 :C5 :C5])
+(def rhythm-time [1/4 1/4 1/4 1/4])
 
-(def part-two [:C4 :E4 :C4 :E4 :C4 :E4])
-(def part-two-time [80 300 80 300 80 300])
+(def part-one [:C4 :E4 :C4 :E4 :C4 :E4])
+(def part-one-time [1/8 1/4 1/8 1/4 1/8 1/4])
 
-(def part-three [:C4 :E4 :F4 :E4])
-(def part-three-time [60 300 300 300])
+(def part-two [:C4 :E4 :F4 :E4])
+(def part-two-time [1/8 1/4 1/4 1/4])
 
-(def part-four [:C5 :C5])
-(def part-four-time [600 600])
+(def part-three [0 :E4 :F4 :E4])
+(def part-three-time [1/8 1/8 1/4 1/4])
+
+(def part-four [0 :E4 :F4 :G4])
+(def part-four-time [1/8 1/4 1/4 1/8])
+
+(def part-five [:E4 :F4 :G4 0])
+(def part-five-time [1/4 1/4 1/8 1/8])
+
+(def part-six [:C5 :C5])
+(def part-six-time [1 1])
+
+(def part-seven [0 0 0 0 :C4 :C4 :C4 0 0 0 0 ])
+(def part-seven-time [1/4 1/4 1/4 1/8 1/16 1/16 1/8 1/8 1/4 1/4 1/4])
+
+(def part-eight [:G4 :F4 :F4])
+(def part-eight-time [1.5 1 1])
+
+(def part-nine [:B5 :G4 :G4 0 0 0 0])
+(def part-nine-time [1/16 1/16 1/8 1/4 1/4 1/4])
 
 (defn player
   [t speeds notes]
@@ -24,15 +42,40 @@
           (sampled-piano (note n)))
       (apply-at t-next #'player [t-next  (next speeds) notes]))))
 
+(def metro (metronome 48))
+
+(defn metro-player
+  [metro beat notes offsets]
+  (let [n (first notes)
+        notes (next notes)
+        offset (first offsets)
+        offsets (next offsets)
+        next-offset (first (next offsets))]
+
+    (at (metro beat) (sampled-piano (note n)))
+    (apply-at (metro (+ beat offset)) #'metro-player [metro (+ beat offset) notes offsets])))
+
 (defn play-measure
-  [times notes]
-  (player (now) (take (count times) (cycle times))
-          (take (count notes) (cycle notes))))
+  [metro notes offsets times]
+  (metro-player metro (metro)
+                (take (* times (count notes)) (cycle notes))
+                (take (* times (count offsets)) (cycle offsets))))
 
 ;(player (now) (take 4 (cycle part-four-time)) (take 4 (cycle part-four)))
+(stop)
+;(metro-player metro (metro) part-two part-two-time)
+(play-measure metro part-nine part-nine-time 2)
 
 (do
-  (play-measure part-one-time part-one)
-  (play-measure part-three-time part-three))
+  ;(play-measure rhythm-time rhythm)
+  ;(play-measure part-one-time part-one)
+  ;(play-measure part-two-time part-two)
+  ;(play-measure part-three-time part-three)
+  ;(play-measure part-four-time part-four)
+  ;(play-measure part-five-time part-five)
+  ;(play-measure part-six-time part-six)
+  )
+
+
 
 ;(play-measure part-one-time part-one)
